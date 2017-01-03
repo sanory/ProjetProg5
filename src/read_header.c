f82ef6beec1ecea1ab0fdbaf7ebd99880dfa13d2
schwingg <georges.schwing@e.ujf-grenoble.fr>
2017-01-03 14:13:17 +0100

    petite correction

--- src/read_header.c
+++ src/read_header.c
@@ -5,7 +5,7 @@
 
 int read_header(FILE* fichier, Elf32_Ehdr* header){ 
 
-	fread(&header,sizeof(Elf32_Ehdr),1,fichier);
+	fread(header,sizeof(Elf32_Ehdr),1,fichier);
 
 	//verification que il s'agit biens d'un fichier ELF 	
 	if (ELFMAG0!=header->e_ident[EI_MAG0] && ELFMAG1!=header->e_ident[EI_MAG1] && ELFMAG2!=header->e_ident[EI_MAG2] && ELFMAG3!=header->e_ident[EI_MAG3])
