6902f026e940a2222ead082f8943bfe353ae1edf
PizzaSansAnchois <moijaimelapizza@gmail.com>
2017-01-03 14:48:54 +0100

    inversement de la valeur ok

--- src/disp_header.c
+++ src/disp_header.c
@@ -4,37 +4,37 @@
 
 
 int display(Elf32_Ehdr *h){
-	int ok=1;
+	int ok=0;
 	printf("En-tête ELF:\nMagique:\t");
 	//NOMBRE MAGIQUE
 		if((h->e_ident[EI_MAG0])==ELFMAG0) {	
 	 	printf("%02x ",ELFMAG0);
 	 	}else{
-			ok=0;
+			ok=1;
 		}
 
 		if((h->e_ident[EI_MAG1])==ELFMAG1) {	
 	 	printf("%02x ",ELFMAG1);
 	 	}else{
-			ok=0;
+			ok=1;
 		}
 
 		if((h->e_ident[EI_MAG2])==ELFMAG2) {	
 	 	printf("%02x ",ELFMAG2);
 	 	}else{
-			ok=0;
+			ok=1;
 		}
 
 		if((h->e_ident[EI_MAG3])==ELFMAG3) {	
 	 	printf("%02x ",ELFMAG3);
 	 	}else{
-			ok=0;
+			ok=1;
 		}
 
 		int i=4;
-		while(0){
+		while(i<16){
+			printf("%02x ",(h->e_ident[i]));
 			i=i+1;
-
 		}
 
 /*
@@ -53,7 +53,7 @@ int display(Elf32_Ehdr *h){
 	 	break;
 
 		default:
-			ok=0;
+			ok=1;
 		}
 
 
