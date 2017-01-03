15d35316a7b63b287a42eb84d074337284f4c500
Arnaudouais <arnaud-av@live.fr>
2017-01-03 14:41:19 +0100

    debug malloc free

--- src/projet.c
+++ src/projet.c
@@ -6,16 +6,15 @@
 
 
 void help(char* commande){
-	printf("Aide de la commande %s. A construire",commande);
+	printf("Aide de la commande %s. A construire\n\n",commande);
+	printf("Options:\n   -h : option par défaut, affiche l'aide\n   -a [argument] : affiche le header du fichier spécifié en argument\n");
 }
 
 int main(int argc, char* argv[]){
 	int opt;
 	//char *option1, *option2;
 	char *afficher_header;
-	FILE *objet1;
-	Elf32_Ehdr *structObjet1;
-	structObjet1=malloc(sizeof(Elf32_Ehdr));
+
 	
 	struct option longopts[] = {
 		{ "afficher_header", required_argument, NULL, 'a' },
@@ -30,6 +29,15 @@ int main(int argc, char* argv[]){
 	//option2 = NULL;
 	afficher_header = NULL;
 
+	if(argc==1){
+		help(argv[0]);
+		exit(2);
+	}
+
+	FILE *objet1;
+	Elf32_Ehdr *structObjet1;
+	structObjet1=malloc(sizeof(Elf32_Ehdr));
+
 	while ((opt = getopt_long(argc, argv, "1:2:a:h", longopts, NULL)) != -1) {
 		switch(opt) {
 		case '1':
@@ -42,15 +50,18 @@ int main(int argc, char* argv[]){
 			break;
 		case 'h':
 			help(argv[0]);
+			free(structObjet1);
 			exit(0);
 		case 'a':
 			afficher_header = optarg;
 			objet1=fopen(afficher_header,"r");
 			read_header(objet1,structObjet1);
+			free(structObjet1);
+			fclose(objet1);
 			break;
 		default:
 			fprintf(stderr, "Unrecognized option %c\n", opt);
-
+			free(structObjet1);
 			exit(1);
 		}
 	}
---
+++ src/projet.h
@@ -0,0 +1,11 @@
+#ifndef ___PROJET___
+#define ___PROJET___
+
+/* La fonction help affiche l'aide de la commande commande*/
+void help(char* commande);
+
+
+
+
+
+#endif
