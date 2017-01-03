debad725cb37acd7813ccc78b6559df79d1b7f54
Thibault VILLERMET <t.hibs@hotmail.fr>
2017-01-03 16:17:22 +0100

    ajout fonction de desaloc

--- src/read_section_header.c
+++ src/read_section_header.c
@@ -3,24 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int desalocSecTable(Elf32_Ehdr *header, Elf32_Shdr **SecHeader){
+	int i,j;
+	if (SecHeader == NULL)
+		free(SecHeader);
+		return 0;
+	for (i=0; i < header->e_shnum; i++) {
+		if (SecHeader[i]==NULL){
+			for (j=i; j>=0; j--)
+				free(SecHeader[j]);
+		}
+	}
+	free(SecHeader);
+	if (SecHeader == 0) {
+		return 0;
+	} else  {
+		return 1;
+	}
+	
+}
 
 int read_section_header(FILE *fichier, Elf32_Ehdr *header, Elf32_Shdr **SecHeader){
-	int i;
+	int i,j;
 if (header->e_shnum==0) {
 	//ya pas de table return 2
-	SecHeader = (Elf32_Shdr **) malloc(header->e_shnum*sizeof(Elf32_Shdr)); 
+	//SecHeader = (Elf32_Shdr **) malloc(header->e_shnum*sizeof(Elf32_Shdr)); 
+	//pas de section mais desalocation du pointeurs des tables de sections
 	return 2;
 } else {
 	//ya au moins une section
 //	uint32_t addrsht=SEEK_END-(header->e_shnum*header->e_shentsize)
 	SecHeader = (Elf32_Shdr **) malloc(header->e_shnum*sizeof(Elf32_Shdr)); 
 	if (SecHeader == NULL)
+		free(SecHeader);
 		return 1;
 	for (i=0; i < header->e_shnum; i++) {
 		SecHeader[i]=(Elf32_Shdr*) malloc(sizeof(Elf32_Shdr));
-		if (SecHeader[i]==NULL)
-			return 1;	
+		if (SecHeader[i]==NULL){
+			for (j=i; j>=0; j--)
+				free(SecHeader[j]);
+			return 1;
+		}
 	}
 	
 	for (i=header->e_shnum; i>0 ; i=-header->e_shnum) {
--- src/read_section_header.h
+++ src/read_section_header.h
@@ -1,3 +1,5 @@
 
+int desalocSecTable(Elf32_Shdr **SecHeader);
+
 
 int read_section_header(FILE *fichier, Elf32_Ehdr *header, Elf32_Shdr **SecHeader);
