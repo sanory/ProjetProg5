a6cc66e47f373ad1d336f30ea0464078cc047371
schwingg <georges.schwing@e.ujf-grenoble.fr>
2017-01-04 09:09:13 +0100

    ajout d'un code erreur pour les fichiers 64 bits qaui ne sont pas supportees

--- src/read_header.c
+++ src/read_header.c
@@ -10,9 +10,12 @@ int read_header(FILE* fichier, Elf32_Ehdr* header){
 	if (ELFMAG0!=header->e_ident[EI_MAG0] && ELFMAG1!=header->e_ident[EI_MAG1] && ELFMAG2!=header->e_ident[EI_MAG2] && ELFMAG3!=header->e_ident[EI_MAG3])
 		return 1;
 	
+	if (header->e_ident[EI_CLASS]==ELFCLASS64)
+		return 2;
+	
 	//verification qu'il s'agit bien d'un fichier 32bit
 	if (header->e_ident[EI_CLASS]!=ELFCLASS32)
-		return 1;
+		return 1; //il ne s'agit pas d'un fichier 32 ou 64 bits
 
 	return 0;
 }
