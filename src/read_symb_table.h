9addf47d50a6370e64f497d4d6afe28eb4c51dcc
schwingg <georges.schwing@e.ujf-grenoble.fr>
2017-01-04 20:11:44 +0100

    fin des fonctions de lectures de la table des symboles reste a implanter dans le projet en n'oubliant pas d'utiliser la desalocation

--- src/read_symb_table.h
+++ src/read_symb_table.h
@@ -1,2 +1,14 @@
 
-int readSymbTable(Elf32_Shdr ** SecHeader, File *fichier);
+/*
+ * 0 ok
+ * 1 pas ok
+ */
+int desalocSymbTable(Elf32_Shdr **SecHeader, Elf32_Sym *** SymbTab);
+
+
+/*
+ * 0 ok
+ * 1 pas ok erreur avec les pointeurs
+ * 2 pas de sections de symboles trouvee
+ */
+int readSymbTable(Elf32_Shdr ** SecHeader,Elf32_Ehdr * header, File *fichier,Elf32_Sym *** Symbtab);
