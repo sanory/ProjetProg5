361839a059dab18c25c96dd0415e889b2ea99d9e
PizzaSansAnchois <moijaimelapizza@gmail.com>
2017-01-05 10:49:52 +0100

    changement nom fonction

--- src/disp_table_symbole.c
+++ src/disp_table_symbole.c
@@ -2,7 +2,7 @@
 #include <elf.h>
 
 
-int display_table(Elf32_Sym **s, Elf32_Shdr ** SecHeader, Elf32_Ehdr *h){
+int display_table_symb(Elf32_Sym **s, Elf32_Shdr ** SecHeader, Elf32_Ehdr *h){
 	int ok=0;
 	
 	//CALCUL NOMBRE D'ENTREES
