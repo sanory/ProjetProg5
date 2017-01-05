7dac4c094194d028b08c6233c65c7ddbcae4893d
Xaalan <mullerl@F212-09.e-im2ag.ujf-grenoble.fr>
2017-01-05 13:08:02 +0100

    fin de display_section.c avec ajout de la recherche du numero par le nom

--- src/display_section.c
+++ src/display_section.c
@@ -1,76 +1,75 @@
 
 #include <elf.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "endian.h"
 
 int display_section(FILE* fichier, int nbSections, Elf32_Shdr ** SecHeader, Elf32_Ehdr *header){
 
 
-
-		printf("shtype=%d\n",SecHeader[nbSections]->sh_type);
-		printf("shflags=%d\n",SecHeader[nbSections]->sh_flags);
-		printf("shaddr=%d\n",SecHeader[nbSections]->sh_addr);
-		printf("shoffset=%d\n",SecHeader[nbSections]->sh_offset);
-		printf("shsize=%d\n",SecHeader[nbSections]->sh_size);
-		printf("shlink=%d\n",SecHeader[nbSections]->sh_link);
-		printf("shinfo=%d\n\n",SecHeader[nbSections]->sh_info);
-
-
 //a ajouter la conversiont du sh_name dnas la table des entetes de sections
 //	printf("Section %s\n",(char *) SecHeader[nbSections]->sh_name);
-        printf("addr |                data");
-        printf("\n------------------------------------------\n");
 
+	if(nbSections>0 && nbSections>header->e_shnum)
+		return 2;
+
+	if (SecHeader[nbSections]->sh_type==SHT_NOBITS)
+		return 1;
+
+	 char * SectNames = NULL;
+
+	SectNames = malloc(SecHeader[header->e_shstrndx]->sh_size);
+	fseek(fichier, SecHeader[header->e_shstrndx]->sh_offset, SEEK_SET);
+  	fread(SectNames, 1, SecHeader[header->e_shstrndx]->sh_size, fichier);
+
+	printf("Vidange hexadécimale de la section «%s»:\n",SectNames + SecHeader[nbSections]->sh_name);
+	printf("addr |                data");
+	printf("\n------------------------------------------\n");
 	int i, k = 0;
 	int nbbits=4;//nombre de bits par paquets a l'ecran
 	unsigned int hex = 0x0;
-	//a tester mais normalement le boutisme na aucune espece d'importance dans cette section
-	/*
-	//verification du boutisme
-	if (header->e_ident[EI_DATA]==ELFDATA2LSB){
-		//presence de petit boutisme conversion obligatoire
-		fseek(fichier,htobe32(SecHeader[nbSections]->sh_offset), SEEK_SET);
-		//ont lit la section tout en formatant l'affichage
-		for (i=htobe32(SecHeader[nbSections]->sh_offset); i <htobe32(SecHeader[nbSections]->sh_offset)+htobe32(SecHeader[nbSections]->sh_size);i+=nbbits){
-			if (k%nbbits==0){
-				printf("\n%04x | ", i);	
-			}
-			k++;
-			fread(&hex, 4, 1, fichier);
-			hex=htobe32(hex);
-			printf("%08x ", hex);
-			hex=0;
-		}
-	}else {
-	*/
-		//pas de conversion a faire
-		
-		if (SecHeader[nbSections]->sh_type==SHT_NOBITS)
-			return 1;
-		
-		
-		
-		
-		
+
+
 		fseek(fichier,SecHeader[nbSections]->sh_offset,SEEK_SET);
 		
 			
-		//on lit tout le contenue de la section
-		printf("%d\n",SecHeader[nbSections]->sh_offset);
-		for (i=SecHeader[nbSections]->sh_offset; i <SecHeader[nbSections]->sh_offset+SecHeader[nbSections]->sh_size;i=i+nbbits){
-                   if (k%nbbits==0){
-						printf("\n%04x | ", i);
-                   }
-                   k++;
-                   fread(&hex, nbbits, 1, fichier);
-                   
-				   printf("%08x ", hex);
-                   //hex=0;
-        }
-	//}
+			//on lit tout le contenue de la section
+	printf("%d\n",SecHeader[nbSections]->sh_offset);
+	for (i=SecHeader[nbSections]->sh_offset; i <SecHeader[nbSections]->sh_offset+SecHeader[nbSections]->sh_size;i=i+nbbits){
+		if (k%nbbits==0){
+			printf("\n%04x | ", i);
+		}
+		k++;
+		fread(&hex, nbbits, 1, fichier);
+		          
+		printf("%08x ", hex);
+		           //hex=0;
+	}
+		//}
 	printf("\n------------------------------------------\n");
-        printf("addr |      data");
-        printf("\n\n");
+	printf("addr |      data");
+	printf("\n\n");
 	
 	return 0;	
+}
+
+int display_section_nom(FILE* fichier, char * nom, Elf32_Shdr ** SecHeader, Elf32_Ehdr *header){
+
+
+	char * SectNames = NULL;
+
+	SectNames = malloc(SecHeader[header->e_shstrndx]->sh_size);
+	fseek(fichier, SecHeader[header->e_shstrndx]->sh_offset, SEEK_SET);
+  	fread(SectNames, 1, SecHeader[header->e_shstrndx]->sh_size, fichier);
+	
+	int i=0;
+
+	while(i<header->e_shnum && !strcmp(SectNames+SecHeader[i]->sh_name,nom)){
+		i=i+1;
+	}
+	if (strcmp(SectNames+SecHeader[i]->sh_name,nom))
+		return display_section(fichier, i, SecHeader, header);
+		
+	return 2;
 }
--- src/display_section.h
+++ src/display_section.h
@@ -1,3 +1,9 @@
 
 //reste a creeer la conversion nom de section
+/*
+* 0 ok
+* 1 erreur le type de la section est SHT_NOBITS
+* 2 erreur le numero ou le nom de la section est invalide
+*/
 int display_section(FILE* fichier, int nbSections, Elf32_Shdr ** SecHeader, Elf32_Ehdr *header);
+int display_section_nom(FILE* fichier, char * nom, Elf32_Shdr ** SecHeader, Elf32_Ehdr *header);
