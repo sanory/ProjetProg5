d09372d4e129451bac1e0999b9b59e994a4749ed
PizzaSansAnchois <moijaimelapizza@gmail.com>
2017-01-05 16:40:26 +0100

    changement arguments

--- src/disp_table_symbole.h
+++ src/disp_table_symbole.h
@@ -1,6 +1,6 @@
 #ifndef __DISP_TABLE_SYMBOLE__
 #define __DISP_TABLE_SYMBOLE__
 
-int display_table_symb(Elf32_Sym **s, Elf32_Shdr ** SecHeader, Elf32_Ehdr *h);
+int display_table_symb(FILE* fichier,Elf32_Sym **s, Elf32_Shdr ** SecHeader, Elf32_Ehdr *h);
 
 #endif
