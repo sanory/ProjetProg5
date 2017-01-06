a4def539823203f791fb2771357fce6a36f3aaf4
schwingg <georges.schwing@e.ujf-grenoble.fr>
2017-01-06 13:32:45 +0100

    suppression inutiles

--- src/read_relocations_table.h
+++
@@ -1,7 +0,0 @@
-
-
-int read_relocations_table(FILE *fichier, Elf32_Rel **** RelTable, Elf32_Rela **** RelaTable, Elf32_Shdr ** SecHeader, Elf32_Ehdr * header);
-
-int desalocRelaTable(Elf32_Rela **** RelaTable, Elf32_Ehdr * header, Elf32_Shdr ** SecHeader);
-
-int desalocRelTable(Elf32_Rel **** RelTable, Elf32_Ehdr * header, Elf32_Shdr ** SecHeader);
