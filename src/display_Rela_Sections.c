38b5501c2abce1e2b499d32ed46f2230eb02896b
Schwingg <georges.schwing@gmail.com>
2017-01-09 00:33:37 +0100

    oubli de fichier lors du precendent commit sur l'affichage des Rela

---
+++ src/display_Rela_Sections.c
@@ -0,0 +1,22 @@
+#include "elfFile.h"
+#include <stdio.h>
+
+int display_rela_section(fichierElf * MonfichierElf) {
+
+    int i,j;
+
+    for (i = 0; i < MonfichierElf->nbRelaSection; i++) {
+        printf("\nSection de readressage '%s' a l'adresse de decalage 0x%x contient %u entrees:\n",
+                MonfichierElf->SectNames + MonfichierElf->secHeader[MonfichierElf->RelaSections[i].numSection].sh_name,
+                MonfichierElf->secHeader[MonfichierElf->RelaSections[i].numSection].sh_offset,
+                MonfichierElf->RelaSections[i].numSection+1);
+        
+        
+        printf("Decalage\tInfo\ttype\t\t\tVal.-sym\tNoms-symboles\n");
+        for (j = 0; j < MonfichierElf->RelaSections[i].RelaTableSize; j++) {
+            printf("%08x  %08x\n",MonfichierElf->RelaSections[i].RelaTable[j].r_offset,
+                    MonfichierElf->RelaSections[i].RelaTable[j].r_info);
+        }
+    }
+    return 1;
+}
---
+++ src/display_Rela_Sections.h
@@ -0,0 +1,6 @@
+#ifndef DISPLAY_RELA_SECTIONS_H
+#define DISPLAY_RELA_SECTIONS_H
+
+int display_rela_section(fichierElf * MonfichierElf);
+#endif	// DISPLAY_RELA_SECTIONS_H
+
