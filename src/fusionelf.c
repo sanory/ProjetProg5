9965b0e14648901b2b395322bf88277e7858b028
Thibault VILLERMET <t.hibs@hotmail.fr>
2017-01-09 10:09:34 +0100

    mise en place des fichier fusion

---
+++ src/fusionelf.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "elfFile.h"
+#include <string.h>
+
+int fusion_elfFile(FILE* fichier, fichierElf  MonfichierElf1, fichierElf  MonfichierElf2, fichierElf * MonfichierElfresultat){
+
+}
