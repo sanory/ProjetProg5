4fe812d99bed002fff172a496756c677f0ed8e85
PizzaSansAnchois <moijaimelapizza@gmail.com>
2017-01-09 10:02:01 +0100

    correction nombre d'entrées

--- src/display_Rel_Sections.c
+++ src/display_Rel_Sections.c
@@ -9,14 +9,27 @@ int display_rel_section(fichierElf * MonfichierElf) {
         printf("\nSection de readressage '%s' a l'adresse de decalage 0x%x contient %u entrees:\n",
                 MonfichierElf->SectNames + MonfichierElf->secHeader[MonfichierElf->RelSections[i].numSection].sh_name,
                 MonfichierElf->secHeader[MonfichierElf->RelSections[i].numSection].sh_offset,
-                MonfichierElf->RelSections[i].numSection + 1);
+                MonfichierElf->RelSections[i].RelTableSize);
 
 
-        printf("Decalage\tInfo\ttype\t\t\tVal.-sym\tNoms-symboles\n");
+        printf("Decalage\tInfo\t\ttype\t\t\tVal.-sym\tNoms-symboles\n");
         for (j = 0; j < MonfichierElf->RelSections[i].RelTableSize; j++) {
-            printf("%08x  %08x \t%d\t\t\t%d\n", MonfichierElf->RelSections[i].RelTable[j].r_offset,
-                    MonfichierElf->RelSections[i].RelTable[j].r_info,
-                    ELF32_R_TYPE(MonfichierElf->RelSections[i].RelTable[j].r_info),
+
+            printf("%08x\t%08x \t",MonfichierElf->RelSections[i].RelTable[j].r_offset,
+                    MonfichierElf->RelSections[i].RelTable[j].r_info);
+
+            switch(ELF32_R_TYPE(MonfichierElf->RelSections[i].RelTable[j].r_info)){
+                case 2:
+                //printf("");
+                break;
+
+                default:
+                printf("\t");
+                break;
+            }
+            
+
+            printf("\t\t\t%d\n",
                     ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info));
         }
     }
