7d9161b8e10bde287b982114de7afa59b89d5993
Schwingg <georges.schwing@gmail.com>
2017-01-09 13:47:49 +0100

    correcitons de beugs anti compilations

--- src/fusionelf.c
+++ src/fusionelf.c
@@ -10,14 +10,14 @@ MonfichierElfresultat->nbSymb = 0;
 //fichier à créer
 MonfichierElfresultat->fichier=fichier;
 //copie du header (à mettre à jour)
-MonfichierElfresultat->header=MonfichierElf1->header;
+MonfichierElfresultat->header=MonfichierElf1.header;
 
-MonfichierElfresultat->nbSections = MonfichierElf1->nbSections+MonfichierElf2->nbSections;
+MonfichierElfresultat->nbSections = MonfichierElf1.nbSections+MonfichierElf2.nbSections;
 
 //calcul du nouveau nombre de sections
-for(i=0; i<MonfichierElf1->nbSections; i++){
-	for(j=0; j<MonfichierElf2->nbSections; j++){
-		if(MonfichierElf1->secHeader[i].sh_type == PROGBITS && MonfichierElf2->secHeader[j].sh_type == PROGBITS && !strcmp(MonfichierElf1->secHeader[i].sh_name+MonfichierElf1->SectNames,MonfichierElf2->secHeader[j].sh_name+MonfichierElf2->SectNames)){
+for(i=0; i<MonfichierElf1.nbSections; i++){
+	for(j=0; j<MonfichierElf2.nbSections; j++){
+		if(MonfichierElf1.secHeader[i].sh_type == SHT_PROGBITS && MonfichierElf2.secHeader[j].sh_type == SHT_PROGBITS && !strcmp(MonfichierElf1.secHeader[i].sh_name+MonfichierElf1.SectNames,MonfichierElf2.secHeader[j].sh_name+MonfichierElf2.SectNames)){
 			MonfichierElfresultat->nbSections--;
 		}
 	}
