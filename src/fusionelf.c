7219c183a44204b4524c4dd734fae6871c0d7d1d
Thibault VILLERMET <t.hibs@hotmail.fr>
2017-01-09 10:21:12 +0100

    ajout return

--- src/fusionelf.c
+++ src/fusionelf.c
@@ -4,4 +4,10 @@
 
 int fusion_elfFile(FILE* fichier, fichierElf  MonfichierElf1, fichierElf  MonfichierElf2, fichierElf * MonfichierElfresultat){
 
+MonfichierElfresultat->nbSections = 0;
+MonfichierElfresultat->nbSymb = 0;
+MonfichierElfresultat->fichier=fichier;
+
+return 1;
+
 }
