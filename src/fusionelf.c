58aeae71dbd9292b416d0e6547f609f50d26c52f
Arnaudouais <arnaud-av@live.fr>
2017-01-10 13:41:56 +0100

    soqkdpzoebk

--- src/fusionelf.c
+++ src/fusionelf.c
@@ -2,17 +2,20 @@
 #include "elfFile.h"
 #include <string.h>
 
-int fusion_elfFile(FILE* fichier, fichierElf  *MonfichierElf1, fichierElf  *MonfichierElf2, fichierElf * MonfichierElfresultat){
+int fusion_elfFile(FILE* fichier, fichierElf  *MonfichierElf1, fichierElf  *MonfichierElf2, fichierElf *MonfichierElfresultat){
 int i,j;
 int * T;
 int arretBoucle = 0;
 Elf32_Off decalage; // décalage par rapport au fichier 1 et non pas au début du fichier
 //init
+
+//1)copie du header (à mettre à jour)
+
 MonfichierElfresultat->nbSections = 0;
 MonfichierElfresultat->nbSymb = 0;
 //fichier à créer
 MonfichierElfresultat->fichier=fichier;
-//copie du header (à mettre à jour)
+
 MonfichierElfresultat->header=MonfichierElf1->header;
 
 MonfichierElfresultat->nbSections = MonfichierElf1->nbSections+MonfichierElf2->nbSections;
@@ -28,14 +31,21 @@ for(i=0; i<MonfichierElf1->nbSections; i++){
 
 MonfichierElfresultat->header.e_shnum = MonfichierElfresultat->nbSections;
 
+//ICI IL FAUT FWRITE LE HEADER !!!!!!! 
+//(e_shoff (debut en tete de sections) n'aura pas la bonne valeur au début, fseek a la fin)
+//je sais pas encore comment faire pour e_shstrndx (debut de la stringtable des noms d'en tete de section)
+
+
+//2) Remplissage des informations des en tetes de sections fusionnées dans une structure, et écriture des sections fusionnées, en meme temps.
+
 MonfichierElfresultat->secHeader =
             malloc(MonfichierElfresultat->nbSections * sizeof (Elf32_Shdr));
     if (MonfichierElfresultat->secHeader == NULL)
         return 2; //erreur dans la creation du pointeur
 
-return 1;
 //allocation d'un tableau de bool
 T=calloc(MonfichierElf2->nbSections ,sizeof (int));
+
 
 for(i=0; i<MonfichierElf1->nbSections; i++){
 	for(j=0; j<MonfichierElf2->nbSections && !(arretBoucle); j++){
@@ -51,7 +61,7 @@ for(i=0; i<MonfichierElf1->nbSections; i++){
 			MonfichierElfresultat->secHeader[i].sh_addralign = MonfichierElf1->secHeader[i].sh_addralign;
 			MonfichierElfresultat->secHeader[i].sh_entsize = MonfichierElf1->secHeader[i].sh_entsize;
 			arretBoucle=1;
-			
+			printf("fusion\n");
 			// a faire fwrite la concatenation des deux sections
 			decalage = decalage + MonfichierElf2->secHeader[j].sh_size;
 			T[j] = 1;
@@ -62,6 +72,7 @@ for(i=0; i<MonfichierElf1->nbSections; i++){
 		MonfichierElfresultat->secHeader[i] = MonfichierElf1->secHeader[i];
 		MonfichierElfresultat->secHeader[i].sh_addr = MonfichierElf1->secHeader[i].sh_addr+decalage;
 		MonfichierElfresultat->secHeader[i].sh_offset = MonfichierElf1->secHeader[i].sh_offset+decalage;
+		printf("assignement 1\n");
 		// a faire fwrite la section
 	}
 	arretBoucle = 0;
@@ -69,6 +80,7 @@ for(i=0; i<MonfichierElf1->nbSections; i++){
 
 for(i=0; i<MonfichierElf2->nbSections; i++){
 	if(T[i]==0){
+		printf("assignement 2\n");
 		MonfichierElfresultat->secHeader[i] = MonfichierElf2->secHeader[i];
 		MonfichierElfresultat->secHeader[i].sh_addr = MonfichierElf2->secHeader[i].sh_addr+decalage;
 		MonfichierElfresultat->secHeader[i].sh_offset = MonfichierElf2->secHeader[i].sh_offset+decalage;
@@ -77,5 +89,5 @@ for(i=0; i<MonfichierElf2->nbSections; i++){
 }
 //penser à e_shoff apres avoir display les sections
 
-
+return 0;
 }
