fe0291b59d5e7e8174ee058c9fb1ee0d8fb9631c
Lucie Muller <mullerl@F214-04.e-im2ag.ujf-grenoble.fr>
2017-01-11 11:30:13 +0100

    affichage des noms de la table de relocation

--- src/display_Rel_Sections.c
+++ src/display_Rel_Sections.c
@@ -1,60 +1,58 @@
 #include "elfFile.h"
 #include <stdio.h>
+#include <string.h>
 
 int display_rel_section(fichierElf * MonfichierElf) {
 
-    int i, j;
+  int i, j;
 
-    for (i = 0; i < MonfichierElf->nbRelSection; i++) {
-        printf("\nSection de readressage '%s' a l'adresse de decalage 0x%x contient %u entrees:\n",
-                MonfichierElf->SectNames + MonfichierElf->secHeader[MonfichierElf->RelSections[i].numSection].sh_name,
-                MonfichierElf->secHeader[MonfichierElf->RelSections[i].numSection].sh_offset,
-                MonfichierElf->RelSections[i].RelTableSize);
+  for (i = 0; i < MonfichierElf->nbRelSection; i++) {
+    printf("\nSection de readressage '%s' a l'adresse de decalage 0x%x contient %u entrees:\n",
+	   MonfichierElf->SectNames + MonfichierElf->secHeader[MonfichierElf->RelSections[i].numSection].sh_name,
+	   MonfichierElf->secHeader[MonfichierElf->RelSections[i].numSection].sh_offset,
+	   MonfichierElf->RelSections[i].RelTableSize);
 
+        
+    printf("Decalage\tInfo\t\ttype\t\t\tVal.-sym\tNoms-symboles\n");
+    for (j = 0; j < MonfichierElf->RelSections[i].RelTableSize; j++) {
 
-        printf("Decalage\tInfo\t\ttype\t\t\tVal.-sym\tNoms-symboles\n");
-        for (j = 0; j < MonfichierElf->RelSections[i].RelTableSize; j++) {
+      printf("%08x\t%08x \t",MonfichierElf->RelSections[i].RelTable[j].r_offset,
+	     MonfichierElf->RelSections[i].RelTable[j].r_info);
 
-            printf("%08x\t%08x \t",MonfichierElf->RelSections[i].RelTable[j].r_offset,
-                    MonfichierElf->RelSections[i].RelTable[j].r_info);
+      switch(ELF32_R_TYPE(MonfichierElf->RelSections[i].RelTable[j].r_info)){
+      case 0:
+	printf("R_ARM_NONE");
+	break;
 
-            switch(ELF32_R_TYPE(MonfichierElf->RelSections[i].RelTable[j].r_info)){
-                case 0:
-                printf("R_ARM_NONE");
-                break;
+      case 2:
+	printf("R_ARM_ABS32");
+	break;
 
-                case 2:
-                printf("R_ARM_ABS32");
-                break;
+      case 28:
+	printf("R_ARM_CALL");
+	break;
 
-                case 28:
-                printf("R_ARM_CALL");
-                break;
+      case 40:
+	printf("R_ARM_V4BX");
+	break;
 
-                case 40:
-                printf("R_ARM_V4BX");
-                break;
-
-                default:
-                printf("%i\t",MonfichierElf->RelSections[i].RelTable[j].r_offset);
-                break;
-            }
+      default:
+	printf("%i\t",MonfichierElf->RelSections[i].RelTable[j].r_offset);
+	break;
+      }
             
-		printf("\t\t%08x",ELF32_R_SYM(MonfichierElf->symTable[j].st_value));
+      printf("\t\t%08x",ELF32_R_SYM(MonfichierElf->symTable[j].st_value));
 
-		//if(ELF32_ST_TYPE(MonfichierElf->symTable[j].st_info)!=3){
-                printf("\t%s\n",MonfichierElf->SymbNames + MonfichierElf->symTable[ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info)].st_name);
-           //}else{
-                /*MonfichierElf->SectNames + MonfichierElf->secHeader[i].sh_name*/
-                /*f->symTable[j].st_shndx*/
-                //printf("\t%s\n",MonfichierElf->SectNames + MonfichierElf->secHeader[MonfichierElf->symTable[j].st_shndx].sh_name);
-                //printf("\tsss\n");
-            //}
+	if(ELF32_ST_TYPE(MonfichierElf->symTable[ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info)].st_info)==3){//verifier si le type est section
+		//printf("\t1\n");
+		//printf("\t%d\n",ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info));
+		//printf("\t%d\n",MonfichierElf->symTable[ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info)].st_shndx);
+		printf("\t%s\n",MonfichierElf->SectNames+MonfichierElf->secHeader[MonfichierElf->symTable[ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info)].st_shndx].sh_name);
 
-
-		//printf("\t%s\n",MonfichierElf->SymbNames + MonfichierElf->symTable[ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info)].st_name);
-		//printf("\t\t%d\n",ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info));
-        }
+	}else{ //si le type n'est pas section
+		printf("\t%s\n",MonfichierElf->SymbNames + MonfichierElf->symTable[ELF32_R_SYM(MonfichierElf->RelSections[i].RelTable[j].r_info)].st_name);
+	}
     }
-    return 1;
+  }
+  return 1;
 }
