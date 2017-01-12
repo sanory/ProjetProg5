669b120df9a351861ff0675336a563ad914ab45c
Schwingg <georges.schwing@gmail.com>
2017-01-12 09:17:58 +0100

    corrections affichages ter

--- src/display_section.c
+++ src/display_section.c
@@ -9,7 +9,6 @@ int display_section(int nbSections, fichierElf * elfFile) {
 
 
     //a ajouter la conversiont du sh_name dnas la table des entetes de sections
-    //	printf("Section %s\n",(char *) secHeader[nbSections]->sh_name);
 
     if (nbSections > elfFile->nbSections)
         return 2;
@@ -18,16 +17,16 @@ int display_section(int nbSections, fichierElf * elfFile) {
         return 1;
 
     if (elfFile->secHeader[nbSections].sh_size == 0) {
-        if (nbSections != 0)
-            printf("la section \" %s \" n'a pas pu etre vidange\n\n", elfFile->SectNames + elfFile->secHeader[nbSections].sh_name);
+        printf("la section \" %s \" n'a pas pu etre vidange\n\n",
+                elfFile->SectNames + elfFile->secHeader[nbSections].sh_name);
         return 3;
     }
 
-    printf("Vidange hexadécimale de la section «%s»:\n", elfFile->SectNames + elfFile->secHeader[nbSections].sh_name);
+    printf("Vidange hexadécimale de la section «%s»:\n",
+            elfFile->SectNames + elfFile->secHeader[nbSections].sh_name);
     printf("    addr    |                data");
     printf("\n--------------------------------------------------");
     int i = 0;
-    //int nbbits=4;//nombre de bits par paquets a l'ecran
     unsigned int hex = 0x0;
 
 
@@ -36,7 +35,6 @@ int display_section(int nbSections, fichierElf * elfFile) {
         if (i % 16 == 0) {
             printf("\n  0x%08x ", i);
         }
-        //fread(&hex, nbbits, 1, elfFile->fichier);
         hex = elfFile->LesSections[nbSections].contenu[i];
         printf("%02x", hex);
         if ((i + 1) % 4 == 0) {
@@ -55,7 +53,8 @@ int display_section_nom(char * nom, fichierElf * elfFile) {
 
     int i = 0;
 
-    while (i < (elfFile->nbSections) - 1 && strcmp(elfFile->SectNames + elfFile->secHeader[i].sh_name, nom)) {
+    while (i < (elfFile->nbSections) - 1 && strcmp(elfFile->SectNames +
+            elfFile->secHeader[i].sh_name, nom)) {
         i = i + 1;
     }
     if (!strcmp(elfFile->SectNames + elfFile->secHeader[i].sh_name, nom))
